ae702429c80791facbcc51b7e561aec4858b02c8
unknown <joel@pcjcollw8.medplaya.com>
2020-02-11 12:39:49 +0100

    PHPMailer

--- src/Application/Adapters/EmailOutputRepository.php
+++ src/Application/Adapters/EmailOutputRepository.php
@@ -3,6 +3,10 @@
 use App\Domain\Interfaces\OutputRepository;
 use App\Domain\Video;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
 class EmailOutputRepository implements OutputRepository
 {
     private $to;
@@ -16,8 +20,39 @@ class EmailOutputRepository implements OutputRepository
 
     public function print(Video $video)
     {
-        $body = $video->getId() . ' .- ' . $video->getTitle();
+        $body = sprintf('<strong>%s.-</strong> %s', $video->getId(), $video->getTitle());
 
-        mail($this->to,$this->subject,$body);
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = getenv('SMTP_HOST');                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = getenv('SMTP_USERNAME');                // SMTP username
+            $mail->Password   = getenv('SMTP_PASSWORD');                // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom(getenv('SMTP_USERNAME'), 'Mailer');
+            $mail->addAddress($this->to);               // Name is optional
+            
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = $this->subject;
+            $mail->Body    = $body;
+
+            $mail->send();
+            
+            echo 'Message has been sent';
+
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
+
     }
 }
